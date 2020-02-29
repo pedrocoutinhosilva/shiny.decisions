4b4365b7185b1ad61f04df2a1c08e919002fc360
Pedro Silva <pedro@appsilon.com>
2020-02-29 20:41:30 +0100

    Generalized state manager.

--- src/app/logic/stateManager.R
+++ src/app/logic/stateManager.R
@@ -3,29 +3,36 @@ import("shiny")
 
 export("stateManager")
 
-
 # Information about the current state of the game
-
-
 stateManager <- R6Class("stateManager",
   public = list(
-    state = NULL,
+    state = reactiveValues(
+      karma = 0,
+      weath = 0,
+      opinion = 0,
+      enviroment = 0,
+      week = 0
+    ),
 
-    updateState = function(newState) {
+    resetState = function() {
+      isolate(self$updateState(list(
+        karma = 50,
+        weath = 50,
+        opinion = 50,
+        enviroment = 50,
 
-      self$state$karma <- self$state$karma + as.numeric(newState$karma)
-      self$state$weath <- self$state$weath + as.numeric(newState$weath)
-      self$state$opinion <- self$state$opinion + as.numeric(newState$opinion)
-      self$state$enviroment <- self$state$enviroment + as.numeric(newState$enviroment)
+        week = 1
+      ), TRUE))
     },
 
-    initialize = function( karma = 50, weath = 30, opinion = 70, enviroment = 40 ) {
-      self$state <- reactiveValues(
-        karma = karma,
-        weath = weath,
-        opinion = opinion,
-        enviroment = enviroment
-      )
+    updateState = function(newState, force = FALSE) {
+      lapply(names(newState), function(attribute) {
+          self$state[[attribute]] <- ifelse (
+            force,
+            newState[[attribute]],
+            self$state[[attribute]] + as.numeric(newState[[attribute]])
+          )
+      })
     }
   )
 )
