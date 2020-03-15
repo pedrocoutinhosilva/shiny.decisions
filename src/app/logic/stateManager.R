53ad5c377dc50eab93ac19558ecba73d543f0e66
Pedro Silva <pedro@appsilon.com>
2020-03-15 17:03:05 +0100

    Clean up state manager.

--- src/app/logic/stateManager.R
+++ src/app/logic/stateManager.R
@@ -3,71 +3,65 @@ import("shiny")
 
 export("stateManager")
 
-# Information about the current state of the game
+# Manages information about the current state of the game
 stateManager <- R6Class("stateManager",
+  private = list(
+    default_state = list(
+      karma = 50,
+      wealth = 50,
+      opinion = 50,
+      enviroment = 50,
+      week = 1
+    )
+  ),
   public = list(
+    # Metrics for game state
     state = reactiveValues(
       karma = 0,
-      weath = 0,
+      wealth = 0,
       opinion = 0,
       enviroment = 0,
       week = 0
     ),
+    # Markers currently in the map
+    markers = list(),
 
-    markers = list(
-      trees = list(),
-      fires = list()
-    ),
-
-    resetState = function() {
-      isolate(self$updateState(list(
-        karma = 50,
-        weath = 50,
-        opinion = 50,
-        enviroment = 50,
-
-        week = 1
-      ), TRUE))
-
-      self$markers = list(
-        trees = list(),
-        fires = list()
-      )
+    # Allows reseting the state of the manager.
+    resetState = function(state = private$default_state) {
+      isolate(self$updateState(state, TRUE))
+      self$markers = list()
     },
 
+    # Checks for death states
     isDeathState = function() {
-      if(self$state$weath < 1 ||
+      if(self$state$wealth < 1 ||
          self$state$opinion < 1 ||
          self$state$enviroment < 1
       ) {
-        return (TRUE)
-      } else {
-        return (FALSE)
+        return(TRUE)
       }
+
+      return(FALSE)
     },
 
-    updateMarkers = function(newMarkers) {
-      self$markers <- newMarkers
-    },
-
+    # If force is false, newState contains the delta values to update
+    # If force is true, newState contains the new values for the state attribute
     updateState = function(newState, force = FALSE) {
       lapply(names(newState), function(attribute) {
-
           self$state[[attribute]] <- ifelse (
             force,
             newState[[attribute]],
             self$state[[attribute]] + as.numeric(newState[[attribute]])
           )
 
+          # Ignore week limits
           if(attribute != "week") {
             if(self$state[[attribute]] < 0)
               self$state[[attribute]] <- 0
             if(self$state[[attribute]] > 100)
-              self$state[[attribute]] <- 100              
+              self$state[[attribute]] <- 100
           }
       })
-
-      print(reactiveValuesToList(self$state))
     }
   )
 )
